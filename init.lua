e13569e58115dfcd57c3f5535ca3eac3732bbfac
Doron Behar <doron.behar@gmail.com>
2018-04-05 18:32:56 +0300

    Use the logger's verbosity and not an external.

--- init.lua
+++ init.lua
@@ -9,21 +9,17 @@ local logger = logging.new(function(self, level, message)
 	return true
 end)
 
-return function (config, verbosity, logs_dir_path, pidfiles_dir_path)
+return function (config, logs_dir_path, pidfiles_dir_path)
 	local logs = logs_dir_path or gears.filesystem.get_cache_dir() .. 'logs/'
 	local tmp = pidfiles_dir_path or os.getenv('XDG_RUNTIME_DIR') .. '/awesome/autostart/' .. os.getenv('XDG_SESSION_ID') .. '/'
 	if gears.filesystem.make_directories(logs) then
-		if verbosity == 2 then
-			logger:debug('Creating directories for logs: ' .. logs)
-		end
+		logger:debug('Creating directories for logs: ' .. logs)
 	else
 		logger:fatal('Couldn\'t create directories for logs, check the permissions and existence of ' .. logs )
 		return
 	end
 	if gears.filesystem.make_directories(tmp) then
-		if verbosity == 2 then
-			logger:debug('Creating directories for pid files: ' .. tmp)
-		end
+		logger:debug('Creating directories for pid files: ' .. tmp)
 	else
 		logger:fatal('Couldn\'t create directories for logs, check the permissions and existence of ' .. logs )
 		return
@@ -32,13 +28,9 @@ return function (config, verbosity, logs_dir_path, pidfiles_dir_path)
 	for i = 1,#config do
 		program[i] = {}
 		program[i].log_fp = logs .. config[i].name .. '.log'
-		if verbosity == 2 then
-			logger:debug('The log file path of ' .. config[i].name .. ' is: ' .. program[i].log_fp)
-		end
+		logger:debug('The log file path of ' .. config[i].name .. ' is: ' .. program[i].log_fp)
 		program[i].pid_fp = tmp .. config[i].name .. '.pid'
-		if verbosity == 2 then
-			logger:debug('The pid file path of ' .. config[i].name .. ' is: ' .. program[i].pid_fp)
-		end
+		logger:debug('The pid file path of ' .. config[i].name .. ' is: ' .. program[i].pid_fp)
 		program[i].spawn = function()
 			local pid = awful.spawn.with_line_callback(config[i].bin, {
 				stdout = function(line)
@@ -51,18 +43,16 @@ return function (config, verbosity, logs_dir_path, pidfiles_dir_path)
 				end,
 				exit = function(reason, code)
 					if reason == 'exit' then
-						logger:info(config[i].name .. ' exited with code: ' .. code)
+						logger:warn(config[i].name .. ' exited with code: ' .. code)
 					elseif reason == 'signal' then
-						logger:info(config[i].name .. ' exited because it recieved signal ' .. code)
+						logger:warn(config[i].name .. ' exited because it recieved signal ' .. code)
 					else
-						logger:info(config[i].name .. ' exited with unknown reason: ' .. code)
+						logger:warn(config[i].name .. ' exited with unknown reason: ' .. code)
 					end
 					if os.remove(program[i].pid_fp) then
-						if verbosity == 2 then
-							logger:debug('Succesfully removed pid file for ' .. config[i].name)
-						end
+						logger:debug('Succesfully removed pid file for ' .. config[i].name)
 					else
-						logger:info('Failed to remove pid file for ' .. config[i].name, {err = true})
+						logger:warn('Failed to remove pid file for ' .. config[i].name, {err = true})
 					end
 				end
 			})
@@ -74,9 +64,7 @@ return function (config, verbosity, logs_dir_path, pidfiles_dir_path)
 	for i = 1,#config do
 		if config[i].delay then
 			spawn_this = function()
-				if verbosity == 2 then
-					logger:debug('Creating timer for configured with delay autostart program ' .. config[i].name)
-				end
+				logger:debug('Creating timer for configured with delay autostart program ' .. config[i].name)
 				program[i].timer = gears.timer({
 					timeout = config[i].delay,
 					callback = program[i].spawn,
@@ -91,9 +79,7 @@ return function (config, verbosity, logs_dir_path, pidfiles_dir_path)
 			if config[i].respawn_on_awesome_restart == true then
 				program[i].pid = io.open(program[i].pid_fp, 'r')
 				local pid = program[i].pid:read("*n")
-				if verbosity == 2 then
-					logger:debug('pid of ' .. config[i].name .. ' is: ' .. pid)
-				end
+				logger:debug('pid of ' .. config[i].name .. ' is: ' .. pid)
 				if awesome.kill(pid, awesome.unix_signal['SIGTERM']) then
 					os.remove(program[i].pid_fp)
 				else
