08fc2b427b001f7c392e0b21a10e5e02607c1a64
Doron Behar <doron.behar@gmail.com>
2018-04-12 00:52:42 +0300

    Divide `ret.run` to several separate functions.
    Make each function receive the `prog` argument like the big old
    `ret.run` in addition to the `pid_fp` - file-path for the pid file.
    Actually create a function for the gears.timer callback since the
    prog.spawn should recieve the arguments `prog` and `pid_fp`.
    Remove support for custom logger per program, just use a good logger
    yourself!

--- init.lua
+++ init.lua
@@ -38,75 +38,71 @@ autostart.new = function(config)
 		ret.logger:fatal('Couldn\'t create directories for logs, check the permissions and existence of ' .. config.log.dir_path)
 		return nil, 'Couldn\'t create directories for logs, check the permissions and existence of ' .. config.log.dir_path
 	end
-	ret.run = function(prog)
-		local logger
-		if type(prog.log) == 'table' then
-			logger = Logger(prog.log.handler, prog.log.settings)
-		else
-			logger = ret.logger
-		end
-		local pid_fp = prog.pid_fp or config.pids_path .. prog.name .. '.pid'
-		logger:debug('The pid file path of ' .. prog.name .. ' is: ' .. pid_fp)
-		prog.spawn = function()
-			local pid = awful.spawn.with_line_callback(prog.bin, {
-				stdout = function(line)
-					logger:info(prog.name .. ':' .. line)
-				end,
-				stderr = function(line)
-					logger:error(prog.name .. ':' .. line)
-				end,
-				exit = function(reason, code)
-					if reason == 'exit' then
-						logger:warn(prog.name .. ' exited with code: ' .. code)
-					elseif reason == 'signal' then
-						logger:warn(prog.name .. ' exited because it recieved signal ' .. code)
-					else
-						logger:warn(prog.name .. ' exited with unknown reason: ' .. code)
-					end
-					if os.remove(pid_fp) then
-						logger:debug('Succesfully removed pid file for ' .. prog.name)
-					else
-						logger:warn('Failed to remove pid file for ' .. prog.name)
-					end
+	ret.spawn = function(prog, pid_fp)
+		local pid = awful.spawn.with_line_callback(prog.bin, {
+			stdout = function(line)
+				ret.logger:info(prog.name .. ':' .. line)
+			end,
+			stderr = function(line)
+				ret.logger:error(prog.name .. ':' .. line)
+			end,
+			exit = function(reason, code)
+				if reason == 'exit' then
+					ret.logger:warn(prog.name .. ' exited with code: ' .. code)
+				elseif reason == 'signal' then
+					ret.logger:warn(prog.name .. ' exited because it recieved signal ' .. code)
+				else
+					ret.logger:warn(prog.name .. ' exited with unknown reason: ' .. code)
 				end
-			})
-			local pid_file = io.open(pid_fp, 'w')
-			pid_file:write(pid)
-			pid_file:close()
-		end
-		if prog.delay then
-			prog.spawn_this = function()
-				logger:debug('Creating timer for configured with delay autostart program ' .. prog.name)
-				local timer = gears.timer({
-					timeout = prog.delay,
-					callback = prog.spawn,
-					single_shot = true
-				})
-				timer:start()
+				if os.remove(pid_fp) then
+					ret.logger:debug('Succesfully removed pid file for ' .. prog.name)
+				else
+					ret.logger:warn('Failed to remove pid file for ' .. prog.name)
+				end
 			end
+		})
+		local pid_file = io.open(pid_fp, 'w')
+		pid_file:write(pid)
+		pid_file:close()
+	end
+	ret.spawn_on_schedule = function(prog, pid_fp)
+		if prog.delay then
+			ret.logger:debug('Creating timer for configured with delay autostart program ' .. prog.name)
+			local timer = gears.timer({
+				timeout = prog.delay,
+				callback = function()
+					ret.spawn(prog, pid_fp)
+				end,
+				single_shot = true
+			})
+			timer:start()
 		else
-			prog.spawn_this = prog.spawn
+			ret.spawn(prog, pid_fp)
 		end
+	end
+	ret.run = function(prog, pid_fp)
 		if gears.filesystem.file_readable(pid_fp) then
 			if prog.respawn_on_awesome_restart == true then
 				local pid_file = io.open(pid_fp, 'r')
 				local pid = pid_file:read("*n")
-				logger:debug('pid of ' .. prog.name .. ' is: ' .. pid)
+				ret.logger:debug('pid of ' .. prog.name .. ' is: ' .. pid)
 				if awesome.kill(pid, awesome.unix_signal['SIGTERM']) then
 					os.remove(pid_fp)
 				else
-					logger:info('killing ' .. prog.name .. ' failed' )
+					ret.logger:info('killing ' .. prog.name .. ' failed' )
 				end
-				prog.spawn_this()
+				ret.spawn_on_schedule(prog, pid_fp)
 			end
 		else
-			logger:error('The pid file of ' .. prog.name .. ' was not found!')
-			prog.spawn_this()
+			ret.logger:error('The pid file of ' .. prog.name .. ' was not found!')
+			ret.spawn_on_schedule(prog, pid_fp)
 		end
 	end
 	ret.run_all = function()
 		for i = 1,#config.programs do
-			ret.run(config.programs[i])
+			local pid_fp = config.programs[i].pid_fp or config.pids_path .. config.programs[i].name .. '.pid'
+			ret.logger:debug('The pid file path of ' .. config.programs[i].name .. ' is: ' .. pid_fp)
+			ret.run(config.programs[i], pid_fp)
 		end
 	end
 	return ret
